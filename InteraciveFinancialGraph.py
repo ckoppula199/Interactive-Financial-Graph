90d4066904a091c5f67dc7f162925588d29fe615
Chak Koppula <chak.koppula199@gmail.com>
2019-07-08 00:07:44 +0530

    Completed project, now lets you choose date range for which to display financial data for

--- InteraciveFinancialGraph.py
+++ InteraciveFinancialGraph.py
@@ -3,16 +3,42 @@ import datetime
 from bokeh.plotting import figure, show, output_file
 import sys
 
+print("Common Company Codes:")
+print("Google, GOOG")
+print("Apple, AAPL")
+print("Amazon, AMZN")
+print("IBM, IBM")
+print("Tesla, TSLA")
+print("AT&T, T")
+
 company_code = input("Enter Company Code: ")
 
-start = datetime.datetime(2019, 4, 1)
-end = datetime.datetime(2019, 7, 1)
+start_year = None
+start_month = None
+start_day = None
+end_year = None
+end_month = None
+end_day = None
+
+try:
+    start_year = int(input("Enter starting year to collect data from: "))
+    start_month = int(input("Enter starting month to collect data from: "))
+    start_day = int(input("Enter starting day to collect data from: "))
+    end_year = int(input("Enter ending year to collect data from: "))
+    end_month = int(input("Enter ending month to collect data from: "))
+    end_day = int(input("Enter ending day to collect data from: "))
+except:
+    print("Invalid input, please only enter numbers corresponding to the year/month/day")
+    sys.exit("Invalid number entry")
+
+start = datetime.datetime(start_year, start_month, start_day)
+end = datetime.datetime(end_year, end_month, end_day)
 df = None
 
 try:
     df = data.DataReader(name=company_code, data_source="yahoo", start=start, end=end)
 except:
-    print("No such company with code: " + company_code)
+    print("No such company with code: " + company_code + "or invalid data given for start and end dates")
     sys.exit("Bad Company Code")
 
 def inc_dec(close, open_):
@@ -27,8 +53,7 @@ def inc_dec(close, open_):
 df["Status"] = [inc_dec(close, open_) for close, open_ in zip(df.Close, df.Open)]
 
 df["Middle"] = (df.Open + df.Close)/2
-df["Height"] = abs(df.Open - df.Close)
-df
+df["Height"] = abs(df.Open - df.Close/2
 
 p=figure(x_axis_type='datetime', width=1000, height=300, sizing_mode="scale_width")
 p.title.text = "Stock Price History: " + company_code
